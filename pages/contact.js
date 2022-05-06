1116b7ee44a45b4e7bb8bc62efee1f9511e15196
Masaya Shida <masayashida@gmail.com>
2022-05-06 10:16:55 +0700

    contact page barebones

--- pages/contact.js
+++ pages/contact.js
@@ -1,29 +1,46 @@
 import G_Map from '../components/g_map'
 import Layout from '../components/layout'
+import styles from '../styles/contact.module.scss'
 
 function Contact({ contactData }) {
     const data = contactData.data.allContactInformations[0]
 
+    const icons = {
+        telephone: '/icons/old-typical-phone.svg',
+        mobile: '/icons/phone-square.svg',
+        fbook: '/icons/facebook-round-color.svg',
+        email: '/icons/envelope.svg'
+    }
+
     return <Layout activePage={"contact"} page_title={"Contact"}>
-        {contactData && 
-        <div>
+    {contactData && 
+    <div className={styles.contact_page_container}>
+        <div className={styles.contact_page_content}>
             <div>
                 <h1>Contact</h1>
                 <ul>
                     <li>
+                        <img src={icons.telephone} alt="Telephone" 
+                        width="16" height="16" />
                         {data.telephone}
                     </li>
                     <li>
+                        <img src={icons.mobile} alt="Mobile" 
+                        width="16" height="16" />
                         {data.mobile}
                     </li>
                     <li>
                         <a href={data.link}>
+                        <img src={icons.fbook} alt="Facebook" 
+                        width="16" height="16" />
                         {data.pageName}
                         </a>
                     </li>
                     <li>
                         <a href={`mailto:${data.email}`}>
-                        {data.email}
+                            <img src={icons.email} alt="Email" 
+                            width="16" height="16" />
+                            {data.email}
                         </a>
                     </li>
                 </ul>
@@ -33,8 +50,9 @@ function Contact({ contactData }) {
                 <p>{data.location}</p>
                 <G_Map />
             </div>
+        </div>
 
-        </div>}
+    </div>}
 
     </Layout>
 }
