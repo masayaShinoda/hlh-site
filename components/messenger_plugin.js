5b22a71763377d689851f4cf6aed4fdb108ad09e
Masaya Shida <masayashida@gmail.com>
2022-05-06 09:23:11 +0700

    added container to messenger button

--- components/messenger_plugin.js
+++ components/messenger_plugin.js
@@ -4,9 +4,12 @@ import styles from '../styles/nav.module.scss'
 export default function MessengerPlugin() {
     const page_id = "153896371634318"
 
-    return <MessengerChat
-        pageId={page_id}
-        themeColor={styles.blue}
-        bottomSpacing={300}
-    />
+    return <div className={styles.messenger_plugin_container}>
+        <MessengerChat
+            pageId={page_id}
+            themeColor={styles.blue}
+            bottomSpacing={300}
+        />
+    </div> 
+    
 }
