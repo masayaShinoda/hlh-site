6faf46d90a2e733f23f6a6181feb40b2c0ffdba0
Masaya Shida <masayashida@gmail.com>
2022-05-18 20:01:53 +0700

    contact info query edit

--- pages/contact.js
+++ pages/contact.js
@@ -76,7 +76,6 @@ export async function getStaticProps() {
                 contactInformation {
                     location
                     mobile
-                    mobileAdditional
                     telephone
                     pageName
                     link
